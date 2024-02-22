544d2cfb6e7e9db4b1a97253900127af97f65539
adanilevics <110359798+adanilevics@users.noreply.github.com>
2024-02-22 16:57:19 +0200

    Update licensekeydemo.py

--- licensekeydemo.py
+++ licensekeydemo.py
@@ -5,7 +5,7 @@ database_path = r'mockdatabase.txt'
 #database_path = 'Licensing'
 #print(database_path+"\mockdatabase.txt")
 print("\nSign up")
-user_email = input("Enter your username :")
+user_name = input("Enter your username :")
 
 #this doesnt change, because it is based on the string that is being encoded, you must implement something for spaces
 #because spaces affect the serial key outcome, any character affects the serial key outcome
@@ -18,15 +18,15 @@ with open(database_path, 'a') as f:
 print("\nSerial key assigned to user : " + str(hashlib.md5(user_email.encode('utf-8')).hexdigest()))
 
 print("\nLog in")
-user_input_email = input("Enter your username :")
-user_input_serial = input("Enter your serial key :")
+user_input_name = input("Enter your username :")
+user_input_name = input("Enter your serial key :")
 
 def search_str(file_path, string_to_search):
     with open(file_path, 'r') as file:
         # read all content of a file
         content = file.read()
         # check if string present in a file
-        if user_input_email in content:
+        if user_input_name in content:
             print('User exists')
             out_search_result = True
         else:
@@ -34,8 +34,8 @@ def search_str(file_path, string_to_search):
             out_search_result = False
     return out_search_result
 
-if search_str(database_path, user_input_email) == True:
-    user_input_serial_check = hashlib.md5(user_email.encode('utf-8')).hexdigest()
+if search_str(database_path, user_input_name) == True:
+    user_input_serial_check = hashlib.md5(user_name.encode('utf-8')).hexdigest()
     print("Correct serial " + user_input_serial_check)
     print("Entered serial " + user_input_serial)
     if user_input_serial == user_input_serial_check:
